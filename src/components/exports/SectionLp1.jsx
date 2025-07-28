29bb86717fd59c10395783eb362672224f61290b
mircomania <mirco.rodriguez@hotmail.com>
2025-07-28 19:53:11 -0400

    2 commit gastos medicos - full section 1 landing page

--- src/components/exports/SectionLp1.jsx
+++ src/components/exports/SectionLp1.jsx
@@ -1,11 +1,25 @@
 import styles from '../../styles/modules/sectionLp1.module.css';
 
+import img1 from '../../assets/images/img-section-1-lp-1.webp';
+
+import { Form } from './Form';
+
 export const SectionLp1 = () => {
     return (
         <section className={styles.sectionContainer}>
-            <header className={styles.sectionHeader}>
-                <h1 className="bold-text-2">hola</h1>
-            </header>
+            <div>
+                <header className={styles.sectionHeader}>
+                    <h1 className="bold-text-2">Protege tu salud y la de tu familia</h1>
+
+                    <h2 className="bold-text">¡Cotiza tu seguro de gastos médicos mayores hoy mismo!</h2>
+                </header>
+
+                <img src={img1} alt="Familia segura y feliz, disfrutando juntos y sin preocupaciones gracias a su seguro de gastos médicos mayores" />
+            </div>
+
+            <div className={styles.form}>
+                <Form />
+            </div>
         </section>
     );
 };
--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,9 +1,12 @@
 import { SectionLp1 } from '../exports/SectionLp1';
+import { SectionLp2 } from '../exports/SectionLp2';
 
 const LandingPage = () => {
     return (
         <main>
             <SectionLp1 />
+
+            <SectionLp2 />
         </main>
     );
 };
